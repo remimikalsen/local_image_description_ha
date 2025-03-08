2dc5c06794dbf533f886dd6617df9e9f779ba9cb
Remi Mikalsen <remimikalsen@gmail.com>
2025-03-08 16:17:22 +0100

    COnstant name fix

--- custom_components/ollama_vision/const.py
+++ custom_components/ollama_vision/const.py
@@ -1,7 +1,7 @@
 DOMAIN = "ollama_vision"
 CONF_OLLAMA_HOST = "ollama_host"
 CONF_OLLAMA_MODEL = "ollama_model"
-CONF_PROMPT = "default_prompt"
+CONF_DEFAULT_PROMPT = "default_prompt"
 CONF_CAMERAS = "cameras"
 
 DEFAULT_PROMPT = "This image is from a security camera located above a door. Describe the gender, age, mood, hair style and the clothes of the person in this image."
